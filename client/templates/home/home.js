5380cde91400fbdd13e34215f5b6ab2e04e805b4
zobe123 <manuel.zobl@gmx.at>
2015-12-28 19:53:44 +0100

    Update home.js

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -8,21 +8,21 @@ Template.home.events({
 
     Meteor.call('checkPlexAuthentication', function(error, data) {
       if (error) {
-      	Bert.alert( 'There was an issue logging in. Please try again!', 'danger');
+      	Bert.alert( 'Fehler. Bitte nochmals versuchen!', 'danger');
       	console.log("Issue checking authentication status: " + error.message);
       } else if (data) {
         Meteor.call('checkPlexUser', plexUsername, function (error, result) {
           if (error) {
-        		Bert.alert( 'There was an issue logging in. Please try again!', 'danger');
+        		Bert.alert( 'Fehler. Bitte nochmals versuchen!', 'danger');
             $('#submitButton').html('<i class="fa fa-user  fa-fw"></i> Sign In');
             Session.setAuth('auth', "false");
           } else if (result === true) {
               Session.setAuth('auth', "true");
               Session.setAuth('user', plexUsername);
-              Bert.alert( 'Successfully logged in!', 'success');
+              Bert.alert( 'Erfolgreich eingeloggt!', 'success');
               Router.go('/search');
           } else if (result === false) {
-        		Bert.alert( 'Wrong username entered. Please try again!', 'danger');
+        		Bert.alert( 'Falscher Benutzername. Bitte nochmals versuchen!', 'danger');
             $('#submitButton').html('<i class="fa fa-user  fa-fw"></i> Sign In');
             Session.setAuth('auth', false);
           }
@@ -30,7 +30,7 @@ Template.home.events({
        } else {
         Session.setAuth('auth', "true");
         Session.setAuth('user', plexUsername);
-        Bert.alert( 'Successfully logged in!', 'success');
+        Bert.alert( 'Erfolgreich eingeloggt!', 'success');
         Router.go('/search');
       }
     });
